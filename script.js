c71d2bed841d97f77ad5d68b26b873afa2e14d4f
TaeSym <shauntesymone@gmail.com>
2023-05-26 00:32:35 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write your JavaScript below this line
+var numberOfHorses = 8;
+console.log(numberOfHorses);
+
+var favoriteFlower = "chrysanthemum";
+console.log("My favorite flower is a " + favoriteFlower);
+
+var birthCity = "Livingston, NJ";
+var birthday = "November 3rd";
+console.log("I was born on " + birthday + " in " + birthCity);
