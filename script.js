46bc4871691dc6b0b36d9ecad7ae2f61842c2d83
TaeSym <shauntesymone@gmail.com>
2023-06-06 19:43:12 -0400

    Added 3 practice exercises

--- script.js
+++ script.js
@@ -29,3 +29,52 @@ var salary = Number(prompt("How much do you make per hour? (ex. 25.50)"));
 var hoursWorked = Number(prompt("How many hours are worked per week? (ex. 40)"));
 
 console.log(`You will make $${(salary * hoursWorked).toFixed(2)} each week before taxes`)
+
+//Skills: conditional statements, comparison operators
+var string = "12";
+var number = 12;
+
+if (string === number) {
+    console.log("Both the values and data types are equal.");
+} else if (string == number) {
+    console.log("Only the values are equal.");
+} else {
+    console.log("Neither the values nor data types are equal.");
+}
+
+//Skills: conditional statements, comparison operators, prompt(), Number()
+var hoursSlept = Number(
+    prompt(
+      "How many hours did you sleep last night (Enter whole number from 0-12 (ex. 8))"
+    )
+  );
+  
+  if (hoursSlept > 12) {
+    console.log("Please enter a number smaller than 12.");
+  } else if (hoursSlept >= 8) {
+    console.log("Yay, you got adequate sleep!");
+  } else if (hoursSlept >= 5) {
+    console.log(
+      "Not bad, but you should consider going to sleep earlier tonight."
+    );
+  } else {
+    console.log("How are you awake right now?? You need more sleep!");
+  }
+  
+// Skills: conditional statements, comparison operators, alert(), prompt(), Number(), arithmetic operators, template literals
+var chocolateWanted = Number(
+    prompt(
+      "How many pieces of chocolate would you like (Enter whole number from 0-10 (ex. 8))"
+    )
+  );
+var leftover = 10 - chocolateWanted
+
+if (leftover === 10) {
+    alert("You must not like chocolate.");
+} else if (leftover >= 5) {
+    alert(`Great job at rationing! You will have ${leftover} pieces of chocolate left for tomorrow`);
+} else if (leftover > 0) {
+    alert(`You'll now only have ${leftover} pieces of chocolate for tomorrow.`);
+} else {
+    alert("Enjoy! But now you won't have any for tomorrow!");
+}
